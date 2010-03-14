92a659e1ccb3bb194142aac184f7d118531d0ea6
zwd0971 <zwd0971@gmail.com>
2010-03-14 15:04:06 +0000

    CustRel部分初始导入

---
+++ RMS-CustRel/src/com/tobacco/custrel/activity/TestActivity.java
@@ -0,0 +1,28 @@
+package com.tobacco.custrel.activity;
+
+import android.app.Activity;
+import android.content.ContentProvider;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class TestActivity extends Activity {
+	
+	
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		Log.i("TestActivity", "Started");
+
+//		setContentView(R.layout.login_dialg);
+//
+//		ContentProvider provider = new AccountProvider();
+//
+//		Button okBtn = (Button) findViewById(R.id.login_btn_ok);
+//		okBtn.setOnClickListener((OnClickListener) loginClickListener);
+
+	}
+
+}
